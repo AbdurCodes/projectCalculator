0d540d55ee92818a0df9a0593455ecea36107d12
AbdurCodes <124054847+AbdurCodes@users.noreply.github.com>
2024-04-15 23:11:27 +0500

    setup one digit calculator that performs basic math operations

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function modulo(num1, num2){
     return num1 % num2;
 }
 
-function operate(operator, num1, num2){
+function operate(num1, operator, num2){
     if (operator === '+') return add(num1, num2)
     else if (operator === '-') return subtract(num1, num2)
     else if (operator === '/') return divide(num1, num2)
@@ -24,17 +24,49 @@ function operate(operator, num1, num2){
     else if (operator === '%') return modulo(num1, num2)
 }
 
+let number1 = 0;
+let number2 = 0;
+let operatorEntered = '';
 
 function numBtnClicked(num) {
-    let exclude = ['0', '+', '-', '/', '*', '%'];
-    if (exclude.includes(display.textContent)) {
+    // let exclude = ['0', '+', '-', '/', '*', '%'];
+    // if (exclude.includes(display.textContent)) {
+    //     display.textContent = '';
+    // }
+    // display.textContent += num;
+
+    if (display.textContent == '0') {
         display.textContent = '';
+        display.textContent += num;
+        number1 = +display.textContent;
+        console.log(number1);
     }
-    display.textContent += num;
-    return display.textContent;
-    // console.log(`${num} is clicked`)
+    if (['+', '-', '/', '*', '%'].includes(display.textContent)) {
+        display.textContent = '';
+        display.textContent += num;
+        number2 = +display.textContent;
+        console.log(number2);
+    }
+
+    // return display.textContent;
 }
 
 function operatorBtnClicked(operator) {
     display.textContent = operator;
+    operatorEntered = display.textContent;
+    // return display.textContent;
+}
+
+function equalBtnClicked() {
+    let result = operate(number1, operatorEntered, number2);
+    display.textContent = result;
+    console.log(result);
+}
+
+
+function allClearBtnClicked() {
+    display.textContent = 0;
+    number1 = 0;
+    number2 = 0;
+    operatorEntered = '';
 }
