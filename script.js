fa1ff62f6a0f963ac3340a6dbdb65ffb22622a94
AbdurCodes <124054847+AbdurCodes@users.noreply.github.com>
2024-04-15 21:52:27 +0500

    Create the functions that populate the display when various buttons are clicked

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const display = document.querySelector('.display');
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -10,11 +12,29 @@ function divide(num1, num2){
 function multiply(num1, num2){
     return num1 * num2;
 }
-
+function modulo(num1, num2){
+    return num1 % num2;
+}
 
 function operate(operator, num1, num2){
     if (operator === '+') return add(num1, num2)
     else if (operator === '-') return subtract(num1, num2)
     else if (operator === '/') return divide(num1, num2)
     else if (operator === '*') return multiply(num1, num2)
+    else if (operator === '%') return modulo(num1, num2)
+}
+
+
+function numBtnClicked(num) {
+    let exclude = ['0', '+', '-', '/', '*', '%'];
+    if (exclude.includes(display.textContent)) {
+        display.textContent = '';
+    }
+    display.textContent += num;
+    return display.textContent;
+    // console.log(`${num} is clicked`)
+}
+
+function operatorBtnClicked(operator) {
+    display.textContent = operator;
 }
